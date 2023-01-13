b65043ec434de5ad5efd717822ecb94af08fa323
Chaoyi Huang <chaoyih@fb.com>
2023-01-13 15:31:26 -0800

    Add Project selector

--- etc/create_gke_infra_manifests.py
+++ etc/create_gke_infra_manifests.py
@@ -1,8 +1,7 @@
-import time
 import yaml
 from pathlib import Path
 import hashlib
-
+import subprocess
 
 class YamlTemplate:
     """
@@ -34,7 +33,7 @@ def create_password_hash(email, password):
     return h.hexdigest()
 
 CB_PATH = Path(".")
-MANIFESTS_DIR = CB_PATH / "etc"/"output" 
+MANIFESTS_DIR = CB_PATH / "etc"/"output"
 
 INSTANCE_ID_CONFIG_MAP_TEMPLATE = CB_PATH / "etc" / "instance-id.yaml"
 INSTANCE_ID_CONFIG_MAP_PATH = MANIFESTS_DIR / "instance-id-config-map.yaml"
@@ -50,20 +49,39 @@ PL_SUBSCRIPTION_PATH = MANIFESTS_DIR / "pl-subscription.yaml"
 INFRA_INSTANCE_TEMPLATE = CB_PATH / "etc" / "infra-instance.yaml"
 INFRA_INSTANCE_PATH = MANIFESTS_DIR / "infra-instance.yaml"
 
-if __name__ == '__main__':
-    print('Welcome to Conversions API Gateway installation wizard!')
-    domain_name = input('Enter your domain name for Conversions API Gateway: \n')  
-    admin_email = input('Enter your account email for Conversions API Gateway: \n')  
-    admin_password = input('Enter your password for Conversions API Gateway: \n')  
-    # domain_name = "gke.chaoyih.cbinternal.com"
-    # admin_email = "chaoyih@fb.com"
-    # admin_password = "1234"
+def set_project_id():
+    process = subprocess.Popen(['gcloud', 'projects', 'list'],
+                        universal_newlines=True,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    # print(stdout, stderr)
+    projects_candidates = stdout.split('\n\n')
 
-    print('Creating Google Kubenetes Engine cluster....')
+    i=1
+    for project in projects_candidates:
+        print('\n-------- Project ', i, '--------\n')
+        print(project.strip())
+        print('\n----------------------------\n')
+        i=i+1
+    project_num = input('Please select your GCP project for installation (Enter a number): \n')
+    ##TODO validate input
+    # print (stdout.split('\n\n'))
+    project_id = 'undefined'
+    project_info_list = projects_candidates[int(project_num)-1].split('\n')
+    for project_info in project_info_list:
+        if project_info.startswith('PROJECT_ID'):
+            project_id = project_info.split(': ')[1]
 
-    print('Installing Conversions API Gateway....')
+    print('You selected Project ID is:', project_id)
+    process = subprocess.Popen(['gcloud', 'config', 'set','project', project_id],
+                        universal_newlines=True,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    print(stdout, stderr)
 
-
+def generate_k8s_manifests(domain_name, admin_email, admin_password):
     YamlTemplate.load(INFRA_INSTANCE_TEMPLATE).build(
         INFRA_INSTANCE_PATH,
         access_token='"' + "access_token" + '"',
@@ -93,3 +111,19 @@ if __name__ == '__main__':
         capig_op_installed="true",
         pl_op_installed="false"
     )
+
+if __name__ == '__main__':
+    print('\n\nWelcome to Conversions API Gateway installation wizard!\n')
+    print('Here are list of your existing GCP projects in your account: \n')
+
+    set_project_id()
+    domain_name = input('Enter your domain name for Conversions API Gateway: \n')
+    admin_email = input('Enter your account email for Conversions API Gateway: \n')
+    admin_password = input('Enter your password for Conversions API Gateway: \n')
+    # domain_name = "gke.chaoyih.cbinternal.com"
+    # admin_email = "chaoyih@fb.com"
+    # admin_password = "1234"
+    generate_k8s_manifests(domain_name, admin_email, admin_password)
+    print('Creating Kubernetes mannifests....')
+
+    print('Installing Conversions API Gateway....')
